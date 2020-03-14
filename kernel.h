4c54b57563c1d1b00a7da1f140553f3bfd76b5d5
kollieartwolf <aclo.create@gmail.com>
2020-03-14 11:25:14 +0300

    Fixed 02 Fixed compilation bug

--- kernel.h
+++ kernel.h
@@ -5,8 +5,6 @@
 
 class Kernel : public QObject {
   Q_OBJECT
-  Q_PROPERTY(
-      QString userName READ userName WRITE setUserName NOTIFY userNameChanged)
   Q_PROPERTY(bool signedIn READ signedIn WRITE setSignedIn)
 public:
   explicit Kernel(QObject *parent = nullptr);
